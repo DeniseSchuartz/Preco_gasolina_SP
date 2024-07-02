8a98b7447a563fe2a598e7499094ecf8d2db534a
denise-schuartz <denise.schuartz@hotmail.com>
2024-07-02 02:33:36 +0000

    Adicionado código para gerar gráfico de preço da gasolina

--- gasolina.py
+++ gasolina.py
@@ -1,14 +1,13 @@
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('gasolina.csv')
+
 with sns.axes_style('whitegrid'):
     sns.lineplot(x='dia', y='venda', data=df)
     plt.xlabel('Dia')
     plt.ylabel('Preço da Gasolina')
     plt.title('Preço da Gasolina por Dia')
-    plt.savefig()
-
-plt.savefig('gasolina.png')
+    plt.savefig('gasolina.png')
+    plt.show()
